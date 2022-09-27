3d7f9bc5902810b9b50872b75508150d58c7625d
drigoni <davider1994@gmail.com>
2022-09-27 15:17:06 +0200

    Add code

---
+++ cluster/debug.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONFIG_FILE=./configs/dh_COCO_concepts_train_sum.yaml
+MODEL_WEIGHTS=./pretrained/dyhead_swint_atss_fpn_2x_ms.pth
+NUM_GPUS=1
+DEEPSETS_EMB='random'
+CONCEPT_APPLY_CONDITION=False
+CONCEPT_APPLY_FILTER=False
+
+python train_net.py --config  ${CONFIG_FILE} \
+                    --num-gpus ${NUM_GPUS} \
+                    MODEL.WEIGHTS ${MODEL_WEIGHTS}  \
+                    DEEPSETS.EMB ${DEEPSETS_EMB} \
+                    CONCEPT.APPLY_CONDITION ${CONCEPT_APPLY_CONDITION} \
+                    CONCEPT.APPLY_FILTER ${CONCEPT_APPLY_FILTER} \
+                    SOLVER.IMS_PER_BATCH 4
