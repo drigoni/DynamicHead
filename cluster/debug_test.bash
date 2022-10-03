1ba688ec1070b0f5fe0601199ef3c41cb24d59e9
drigoni <davider1994@gmail.com>
2022-10-03 19:59:35 +0200

    Add new files and refactory of the results

--- cluster/debug_test.bash
+++ cluster/debug_test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIG_FILE=./configs/COCO/retinanet/retinanet_r50_fpn_COCO_concepts_test_add.yaml
-OUTPUT_DIR="./results_new/debug/"
+OUTPUT_DIR="./results/debug/"
 
 python train_net.py --config  ${CONFIG_FILE} \
                     --num-gpus 1 \
