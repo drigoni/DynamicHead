f1534039de14692d5dcd13cefe2a1b8043451036
drigoni <davider1994@gmail.com>
2022-10-16 13:28:23 +0200

    Refactory

---
+++ BACKUPS/cluster_FULL_COCO/debug_test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONFIG_FILE=./configs/COCO/dh/dh_swint_fpn_COCO_test.yaml
+OUTPUT_DIR="./results/debug/"
+
+python train_net.py --config  ${CONFIG_FILE} \
+                    --num-gpus 1 \
+                    --eval-only \
+                    OUTPUT_DIR ${OUTPUT_DIR} \
+                    SOLVER.IMS_PER_BATCH 16 
+                    # EVALUATOR_TYPE 'GTFilter' # ['default', 'GTFilter']
+
+
+
+
